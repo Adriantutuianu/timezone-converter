714e58368981467923ef45dd348fb420ce0d5f04
Adrian <adriantutuianu01@gmail.com>
2023-02-03 10:46:12 +0000

    Saved in state timezone

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./content.css";
 import timezones from "../timezones.json";
+import { useState } from "react";
 
 const Content = () => {
   console.log(timezones);
-  return <div>Content</div>;
+
+  const [timezonesList, setTimezonesList] = useState(timezones);
+
+  console.log("bau", timezonesList);
+  return (
+    <div>
+      <select name="cars" id="cars">
+        {/* <option value="volvo">set</option> */}
+      </select>
+    </div>
+  );
 };
 
 export default Content;
