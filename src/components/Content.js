c70b0fa7ec2867d0d63251b98a9ef36cf96ba839
Adrian <adriantutuianu01@gmail.com>
2023-02-03 11:06:31 +0000

    Fixed key-index

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,16 +4,17 @@ import timezones from "../timezones.json";
 import { useState } from "react";
 
 const Content = () => {
-  console.log(timezones);
-
   const [timezonesList, setTimezonesList] = useState(timezones);
 
-  console.log("bau", timezonesList);
   return (
     <div>
       <select name="timezone" id="timezone">
-        {timezonesList.map((item) => {
-          return <option value={item.utc}>{item.label}</option>;
+        {timezonesList.map((item, index) => {
+          return (
+            <option key={index} value={item.utc}>
+              {item.label}
+            </option>
+          );
         })}
       </select>
     </div>
