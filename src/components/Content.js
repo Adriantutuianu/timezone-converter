9292c6d1ea49f67e5a3f23a3e698bc1ee3b116a0
Adrian <adriantutuianu01@gmail.com>
2023-02-04 21:21:53 +0000

    Added style to content

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,17 +7,19 @@ const Content = () => {
   const [timezonesList, setTimezonesList] = useState(timezones);
 
   return (
-    <div>
-      <select name="timezone" id="timezone">
-        {timezonesList.map((item, index) => {
-          return (
-            <option key={index} value={item.utc}>
-              {item.label}
-            </option>
-          );
-        })}
-      </select>
-    </div>
+    <main className="content">
+      <div>
+        <select name="timezone" id="timezone">
+          {timezonesList.map((item, index) => {
+            return (
+              <option key={index} value={item.utc}>
+                {item.label}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </main>
   );
 };
 
